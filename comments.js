76a72c820c7b3d72a25596bfba7d69ae6c06673a
John Schachte <89204366+JohnSchachte@users.noreply.github.com>
2024-01-19 01:43:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,61 +1,20 @@
-// create webserver
-// 2017-08-01    PV
+// create a webserver with express router and commentController
+// to handle the comments routes
+// 1. create a router
+// 2. create a controller
+// 3. export the router
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const commentController = require('../controllers/commentController');
 
-var db = require('../db');
+const commentRouter = express.Router();
 
-/* GET comments listing. */
-router.get('/', function (req, res, next) {
-    var sql = "select * from comments";
-    db.query(sql, function (err, data, fields) {
-        if (err) throw err;
-        res.json(data);
-    });
-});
+commentRouter.get('/', commentController.index);
+commentRouter.get('/new', commentController.new);
+commentRouter.get('/:id', commentController.show);
+commentRouter.post('/', commentController.create);
+commentRouter.get('/:id/edit', commentController.edit);
+commentRouter.put('/:id', commentController.update);
+commentRouter.delete('/:id', commentController.delete);
 
-/* GET comments for given id */
-router.get('/:id', function (req, res, next) {
-    var id = req.params.id;
-    var sql = "select * from comments where id = ?";
-    db.query(sql, [id], function (err, data, fields) {
-        if (err) throw err;
-        res.json(data);
-    });
-});
-
-/* POST comments */
-router.post('/', function (req, res, next) {
-    var title = req.body.title;
-    var body = req.body.body;
-    var sql = "insert into comments (title, body) values (?, ?)";
-    db.query(sql, [title, body], function (err, data, fields) {
-        if (err) throw err;
-        res.json({ message: "comment added" });
-    });
-});
-
-/* PUT comments */
-router.put('/:id', function (req, res, next) {
-    var id = req.params.id;
-    var title = req.body.title;
-    var body = req.body.body;
-    var sql = "update comments set title = ?, body = ? where id = ?";
-    db.query(sql, [title, body, id], function (err, data, fields) {
-        if (err) throw err;
-        res.json({ message: "comment updated" });
-    });
-});
-
-/* DELETE comments */
-router.delete('/:id', function (req, res, next) {
-    var id = req.params.id;
-    var sql = "delete from comments where id = ?";
-    db.query(sql, [id], function (err, data, fields) {
-        if (err) throw err;
-        res.json({ message: "comment deleted" });
-    });
-});
-
-module.exports = router;
+module.exports = commentRouter;
