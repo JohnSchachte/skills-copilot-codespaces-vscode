48ca2b36bdbed8f9582f2113a2eb92a4249738fc
John Schachte <89204366+JohnSchachte@users.noreply.github.com>
2024-01-19 01:40:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,60 @@
-// create a webserver
+// create webserver
+// 2017-08-01    PV
+
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-var comment = require('./comment');
 
-// CREATES A NEW COMMENT
-router.post('/create', function (req, res) {
-    comment.create({
-            name : req.body.name,
-            comment : req.body.comment,
-            date : req.body.date,
-            upvotes : req.body.upvotes,
-            downvotes : req.body.downvotes
-        }, 
-        function (err, comment) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(comment);
-        });
+var db = require('../db');
+
+/* GET comments listing. */
+router.get('/', function (req, res, next) {
+    var sql = "select * from comments";
+    db.query(sql, function (err, data, fields) {
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
-// RETURNS ALL THE COMMENTS IN THE DATABASE
-router.get('/', function (req, res) {
-    comment.find({}, function (err, comments) {
-        if (err) return res.status(500).send("There was a problem finding the comments.");
-        res.status(200).send(comments);
+/* GET comments for given id */
+router.get('/:id', function (req, res, next) {
+    var id = req.params.id;
+    var sql = "select * from comments where id = ?";
+    db.query(sql, [id], function (err, data, fields) {
+        if (err) throw err;
+        res.json(data);
+    });
+});
+
+/* POST comments */
+router.post('/', function (req, res, next) {
+    var title = req.body.title;
+    var body = req.body.body;
+    var sql = "insert into comments (title, body) values (?, ?)";
+    db.query(sql, [title, body], function (err, data, fields) {
+        if (err) throw err;
+        res.json({ message: "comment added" });
+    });
+});
+
+/* PUT comments */
+router.put('/:id', function (req, res, next) {
+    var id = req.params.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var sql = "update comments set title = ?, body = ? where id = ?";
+    db.query(sql, [title, body, id], function (err, data, fields) {
+        if (err) throw err;
+        res.json({ message: "comment updated" });
+    });
+});
+
+/* DELETE comments */
+router.delete('/:id', function (req, res, next) {
+    var id = req.params.id;
+    var sql = "delete from comments where id = ?";
+    db.query(sql, [id], function (err, data, fields) {
+        if (err) throw err;
+        res.json({ message: "comment deleted" });
     });
 });
 
