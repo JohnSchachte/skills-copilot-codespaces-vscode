ee0c8943af31b52e0092325974cc108e656d3dd8
John Schachte <89204366+JohnSchachte@users.noreply.github.com>
2024-01-19 01:29:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        skills: ['js', 'css', 'html']
+    };
+    console.log(member.name + ' has skills: ' + member.skills.join(', '));
+}
+
--- members.js
+++
@@ -1,8 +0,0 @@
-function skillMembers() {
-    return [
-        {name: 'John', skill: 'JavaScript'},
-        {name: 'Jane', skill: 'HTML'},
-        {name: 'Jack', skill: 'CSS'}
-    ];
-
-}
